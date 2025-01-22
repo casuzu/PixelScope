da7304466a284ea2a0debec56fbce22c3d8ab2f3
Chibunkem Asuzu <c.masuzu@gmail>
2025-01-22 12:09:05 -0500

    Reorganized and simplified the LineClass. Deleted the get and set for starting and ending points methods. Removed set_line_dist method. Added calculate_line_dist method instead.

--- LineClass.py
+++ LineClass.py
@@ -4,30 +4,20 @@ import math
 class MyLine:
     line_id = 0
 
-    def __init__(self, starting_point):
-        self._id = self.line_id + 1
-        self._starting_point = starting_point
-        self._ending_point = None
-        self._line_dist = 0
-        self._color = None
-        self._type = None
-        self._angle =None
+    def __init__(self, starting_point, ending_point, angle, line_type, line_color):
+        MyLine.line_id += 1
+        self._id = MyLine.line_id
+        self.starting_point = starting_point
+        self.ending_point = ending_point
+        self._line_dist = None
+        self._color = line_color
+        self._type = line_type
+        self._angle = angle
 
+        self.calculate_line_dist()
 
-    def set_starting_point(self, new_starting_point):
-        self._starting_point = new_starting_point
-
-    def get_starting_point(self):
-        return self._starting_point
-
-    def set_ending_point(self, new_ending_point):
-        self._ending_point = new_ending_point
-
-    def get_ending_point(self):
-        return self._ending_point
-
-    def set_line_dist(self):
-        self._line_dist = math.dist(self._starting_point, self._ending_point)
+    def calculate_line_dist(self):
+        self._line_dist = math.dist(self.starting_point, self.ending_point)
 
     def get_line_dist(self):
         return self._line_dist
