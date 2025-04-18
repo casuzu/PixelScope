fa576ad481bd51f21b66820efa6e795365ac5239
Chibunkem Asuzu <c.masuzu@gmail>
2025-04-18 10:34:56 -0400

    Pycharm space adjustments

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ from SplineMaster import MySpline
 import tkinter as tk
 import EdgedMenu
 
-
-
 root = tk.Tk()
 SCREEN_WIDTH = root.winfo_screenwidth()
 SCREEN_HEIGHT = root.winfo_screenheight()
 root.destroy()
 
+
 def send_to_file(coordinates, file_name):
-    #header = ['X','Y']
-    #points_file = open('points.csv', 'w+', newline='')
+    # header = ['X','Y']
+    # points_file = open('points.csv', 'w+', newline='')
 
     filename = file_name + ".csv"
 
@@ -23,6 +22,7 @@ def send_to_file(coordinates, file_name):
         csv_writer.writerows([["[X1, Y1]", "[X2, Y2]"]])
         csv_writer.writerows(coordinates)
     csv_file.close()
+
 
 def save_analysis_img(save_img, img_name, save_path):
     try:
@@ -50,12 +50,15 @@ def images_same_size(img1, img2):
     else:
         return False
 
+
 def img_up_resizer(image, img_screen_percent):
     new_width = int(img_screen_percent * SCREEN_HEIGHT)
     new_height = new_width
     new_size = (new_width, new_height)
     resized_img = cv2.resize(image, new_size, interpolation=cv2.INTER_LINEAR)
     return resized_img
+
+
 def main_menu():
     print("What mode would you like to run?:")
     print("Press l for Straight Line Mode.")
@@ -66,9 +69,7 @@ def main_menu():
     select_main_menu = False
 
 
-
-
-#img_screen_ratio = EdgedMenu.IMG_SCREEN_RATIO
+# img_screen_ratio = EdgedMenu.IMG_SCREEN_RATIO
 img = EdgedMenu.original_img_filepath
 img_edged = EdgedMenu.final_edged_img_filepath
 
@@ -88,10 +89,10 @@ if images_same_size(img_edged, img):
     while running:
         key = cv2.waitKey(1)
 
-        if select_main_menu or key == ord('m')or key == ord('M'):
+        if select_main_menu or key == ord('m') or key == ord('M'):
             main_menu()
 
-        if key == ord('l')or key == ord('L'):
+        if key == ord('l') or key == ord('L'):
             spline.reset_all()
             print("========================")
             print("STRAIGHT LINE")
@@ -105,10 +106,10 @@ if images_same_size(img_edged, img):
             spline.modeselect = "Points"
             spline.mode()
 
-        elif key == ord('c')or key == ord('C'):
-            if spline.CALIBRATION_COMPLETE: #If calibration is already done
-                select_main_menu = True #call main menu
-                spline.CALIBRATION_COMPLETE = False #Reset the calibration mode
+        elif key == ord('c') or key == ord('C'):
+            if spline.CALIBRATION_COMPLETE:  # If calibration is already done
+                select_main_menu = True  # call main menu
+                spline.CALIBRATION_COMPLETE = False  # Reset the calibration mode
                 spline.modeselect = None
             else:
                 spline.reset_all()
@@ -116,8 +117,9 @@ if images_same_size(img_edged, img):
                 print("CALIBRATION MODE")
                 print("========================")
 
-                spline.total_calibration_num = int(input("Please enter the number(integer) of calibration lines in order"
-                                                         " to calibrate the pixel to mm relationship:  "))
+                spline.total_calibration_num = int(
+                    input("Please enter the number(integer) of calibration lines in order"
+                          " to calibrate the pixel to mm relationship:  "))
                 print("Press the 'm' key after 'CALIBRATION COMPLETED' message to return to the main menu.")
                 spline.modeselect = "Calibration"
                 spline.mode()
@@ -130,12 +132,12 @@ if images_same_size(img_edged, img):
                 print("Lines = {}".format(lines_list))
 
                 vertic_lines_list = [[i.starting_point, i.ending_point]
-                                       for i in spline.get_lines()
-                                       if i.get_type() =="Vertical"]
+                                     for i in spline.get_lines()
+                                     if i.get_type() == "Vertical"]
 
                 horizon_lines_list = [[i.starting_point, i.ending_point]
-                                       for i in spline.get_lines()
-                                       if i.get_type() == "Horizontal"]
+                                      for i in spline.get_lines()
+                                      if i.get_type() == "Horizontal"]
 
                 print("\nVertical Lines = {}".format(vertic_lines_list))
                 print("\nHorizontal Lines = {}".format(horizon_lines_list))
@@ -143,12 +145,11 @@ if images_same_size(img_edged, img):
                 send_to_file(vertic_lines_list, "Vertical_Lines")
                 send_to_file(horizon_lines_list, "Horizontal_Lines")
 
-            #analy_img_name = input("What name would you like to give the analysis image?:")
-            #analy_img_location = input("Where do you want to store the analysis image?:")
+            # analy_img_name = input("What name would you like to give the analysis image?:")
+            # analy_img_location = input("Where do you want to store the analysis image?:")
 
-            #save_analysis_img(spline.clone, analy_img_name, analy_img_location)
+            # save_analysis_img(spline.clone, analy_img_name, analy_img_location)
             cv2.destroyAllWindows()
             exit(0)
 else:
     print("Images are not the same size.")
-
