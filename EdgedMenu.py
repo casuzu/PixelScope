f892d921592584052da40aed3beb688780250d93
Chibunkem Asuzu <c.masuzu@gmail>
2025-12-24 10:30:55 -0500

    # MAJOR COMMIT Put EdgedMenu.py display outputs on a frame on the main.py tk.root -This is part of the tk as the display added feature

--- EdgedMenu.py
+++ EdgedMenu.py
@@ -4,15 +4,6 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-# Create the display window and title
-root = tk.Tk()
-root.title("PixelScope")
-
-SCREEN_WIDTH = root.winfo_screenwidth()
-SCREEN_HEIGHT = root.winfo_screenheight()
-
-root.minsize(SCREEN_WIDTH, SCREEN_HEIGHT)
-
 
 def convert_to_tk_img(opencv_img):  # Converts opencv image to tkinter image
     RGB_img = cv2.cvtColor(opencv_img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
@@ -38,21 +29,41 @@ def convert_to_opencv_img(tk_image):  # Converts tkinter image to opencv image
     return open_cv_image
 
 
-def close_window(event=None):  # Close the display window and ends the program
-    root.destroy()
-
-
 class MyEdgedImageMaker:
-    def __init__(self, img_screen_ratio):
+    def __init__(self, img_screen_ratio, root, on_continue):
+        # if this frame is completed, use continue to move to next frame in main.py
+        self.on_continue = on_continue  # store callback for main.py
 
-        # Original Image
+        # Window frame setup
+        self.root = root
+        self.frame = tk.Frame(root)
+
+        # Change the background color of the frame using configure
+        self.frame.configure(bg='lightblue')
+        self.frame.pack(fill="both", expand=True)
+
+        # Half the screen to be used as images display
         self.IMG_SCREEN_RATIO = img_screen_ratio
+
+        # Create a main frame for buttons
+        self.button_frame = tk.Frame(self.frame)
+        self.button_frame.pack(side=tk.RIGHT, padx=20, pady=20, fill=tk.Y)
+
+        # Used to check if the save button has already been added to the frame
+        self.save_button_created = False
+
+        # Create a label for the original image
         self.img_label = None
 
-        # Edged Image
+        # Create a label for the Edged Image
         self.edged_img_label = None
 
+        # Used to resized images to the target size
         self.img_target_size = None
+
+        # Canny Edge sliders
+        self.low_threshold_slider = None
+        self.high_threshold_slider = None
 
         # The values for canny low and high thresholds were choosen experimentally.
         self.canny_low_threshold = 40
@@ -62,8 +73,24 @@ class MyEdgedImageMaker:
         self.img_file_path = ""
         self.edged_img_file_path = ""
 
+        # Update Final images
+        self.final_edged_img = None
+        self.final_original_img = None
+        self.update_final_images()
+
+        # show SAVE button only after an image is initially uploaded
+        self.show_save_button = False
+
+        self.run()
+
+    def close_window_frame(self):  # Close the display window frame
+        self.update_final_images()
+        self.frame.destroy()
+        self.on_continue(self.final_original_img, self.final_edged_img)
+
     def target__resizer(self, col, row):
         resize_factor = col / float(row)
+        SCREEN_WIDTH = self.root.winfo_screenwidth()
 
         # 1/3 of the screenwidth is used as the width of the image
         target_width = (self.IMG_SCREEN_RATIO * SCREEN_WIDTH) / 2.0
@@ -106,12 +133,12 @@ class MyEdgedImageMaker:
         self.save_resized_uploaded_img()
 
         # If the image is uploaded, show the canny threshold sliders
-        low_threshold_slider.pack(side=tk.TOP, anchor="e")
-        high_threshold_slider.pack(side=tk.TOP, anchor="e")
+        self.low_threshold_slider.pack(side=tk.TOP, anchor="e")
+        self.high_threshold_slider.pack(side=tk.TOP, anchor="e")
 
-        # Displays and move the button to a new position after image is uploaded
-        # img_btn_save.place(x=widget_xpos + 75, y=widget_ypos + 30)
-        img_btn_save.pack(pady=25, fill=tk.X)
+        # Displays the SAVE button after an image is initially been uploaded
+        # img_btn_save.pack(pady=25, fill=tk.X)
+        self.run(add_save_button=True)
 
         # Calls edge maker after image is uploaded
         self.edge_maker(0)
@@ -132,10 +159,6 @@ class MyEdgedImageMaker:
 
         # Resize the image
         img_selected = cv2.resize(img_selected, self.img_target_size, interpolation=cv2.INTER_CUBIC)
-
-        # self.img_target_size = (int(img_selected.shape[0]), int(img_selected.shape[1]))
-        # Resize the image
-        # img_selected = cv2.resize(img_selected, self.img_target_size, interpolation=cv2.INTER_CUBIC)
 
         # Convert to tkinter image
         tk_img = convert_to_tk_img(img_selected)
@@ -186,8 +209,8 @@ class MyEdgedImageMaker:
         grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Get current values of the sliders (lower and upper thresholds)
-        low_thresh = low_threshold_slider.get()
-        high_thresh = high_threshold_slider.get()
+        low_thresh = self.low_threshold_slider.get()
+        high_thresh = self.high_threshold_slider.get()
 
         # Apply edge detection techniques: gaussian blur and canny edge detection
         grey_img_blur = cv2.GaussianBlur(grey_img, (3, 3), 0)
@@ -254,72 +277,58 @@ class MyEdgedImageMaker:
             save_edged_pil_image.save(file_path)
             self.edged_img_file_path = file_path
             print("Edged image saved at ", file_path)
-            close_window()
+            self.close_window_frame()
 
+    def run(self, add_save_button=False):
+        if not add_save_button:
+            # Create sliders (trackbars) for the thresholds
+            low_threshold = self.canny_low_threshold
+            self.low_threshold_slider = tk.Scale(self.frame, from_=0, to=255, orient='horizontal',
+                                                 label="Lower Threshold",
+                                                 command=self.edge_maker)
+            self.low_threshold_slider.set(low_threshold)
 
-# Start the loop
-# Half of the entire screen width was choosen as the images playground.
-IMG_SCREEN_RATIO = 0.5
-edged_img_maker = MyEdgedImageMaker(IMG_SCREEN_RATIO)
+            high_threshold = self.canny_high_threshold
+            self.high_threshold_slider = tk.Scale(self.frame, from_=0, to=255, orient='horizontal',
+                                                  label="Upper Threshold",
+                                                  command=self.edge_maker)
+            self.high_threshold_slider.set(high_threshold)
 
-# Use the computer's screen max width and height as display window
-SCREEN_WIDTH_string = str(SCREEN_WIDTH)
-SCREEN_HEIGHT_string = str(SCREEN_HEIGHT)
-# print("screen size  = [", SCREEN_WIDTH_string, " ", SCREEN_HEIGHT_string, "]")
-root.geometry(SCREEN_WIDTH_string + "x" + SCREEN_HEIGHT_string)
+            # Show image and set image position
+            self.img_label = tk.Label(self.frame)
+            self.img_label.pack(side=tk.LEFT, anchor="n")
 
-# Change the background color using configure
-root.configure(bg='lightblue')
+            # Show edged image and set edged image position
+            self.edged_img_label = tk.Label(self.frame)
+            self.edged_img_label.pack(side=tk.LEFT, anchor="n")
 
-# Create sliders (trackbars) for the thresholds
-low_threshold = edged_img_maker.canny_low_threshold
-low_threshold_slider = tk.Scale(root, from_=0, to=255, orient='horizontal', label="Lower Threshold",
-                                command=edged_img_maker.edge_maker)
-low_threshold_slider.set(low_threshold)
+            # Show upload button and set the button position
+            img_btn_upload = tk.Button(self.button_frame, text="UPLOAD NEW IMAGE", command=self.upload_new_image)
+            img_btn_upload.pack(pady=25, fill=tk.X)
 
-high_threshold = edged_img_maker.canny_high_threshold
-high_threshold_slider = tk.Scale(root, from_=0, to=255, orient='horizontal', label="Upper Threshold",
-                                 command=edged_img_maker.edge_maker)
-high_threshold_slider.set(high_threshold)
+            # Show load image 1 button and set the button position
+            img1_btn_load = tk.Button(self.button_frame, text="LOAD SAVED RESIZED IMAGE", command=self.load_image1)
+            img1_btn_load.pack(pady=25, fill=tk.X)
 
-# Show image and set image position
-edged_img_maker.img_label = tk.Label(root)
-edged_img_maker.img_label.pack(side=tk.LEFT, anchor="n")
+            # Show load image 2 button and set the button position
+            img2_btn_load = tk.Button(self.button_frame, text="LOAD SAVED CANNY IMAGE", command=self.load_image2)
+            img2_btn_load.pack(pady=25, fill=tk.X)
 
-# Show edged image and set edged image position
-edged_img_maker.edged_img_label = tk.Label(root)
-edged_img_maker.edged_img_label.pack(side=tk.LEFT, anchor="n")
+            img_btn_continue = tk.Button(self.button_frame, text="CONTINUE", command=self.close_window_frame)
+            img_btn_continue.pack(pady=25, fill=tk.X)
 
-# Create a main frame for buttons
-button_frame = tk.Frame(root)
-button_frame.pack(side=tk.RIGHT, padx=20, pady=20, fill=tk.Y)
+        elif not self.save_button_created:
+            # Show save button and set the button position
+            img_btn_save = tk.Button(self.button_frame, text="SAVE "
+                                                             "CANNY IMAGE", command=self.save_edged_img)
+            # Displays and move the button to a new position after image is uploaded
+            img_btn_save.pack(pady=25, fill=tk.X)
 
-# Show upload button and set the button position
-img_btn_upload = tk.Button(button_frame, text="UPLOAD NEW IMAGE", command=edged_img_maker.upload_new_image)
-img_btn_upload.pack(pady=25, fill=tk.X)
+            self.save_button_created = True
 
-# Show load image 1 button and set the button position
-img1_btn_load = tk.Button(button_frame, text="LOAD SAVED RESIZED IMAGE", command=edged_img_maker.load_image1)
-img1_btn_load.pack(pady=25, fill=tk.X)
+    def update_final_images(self):
+        self.final_original_img = self.get_original_img()
+        self.final_edged_img = self.get_edged_img()
 
-# Show load image 2 button and set the button position
-img2_btn_load = tk.Button(button_frame, text="LOAD SAVED CANNY IMAGE", command=edged_img_maker.load_image2)
-img2_btn_load.pack(pady=25, fill=tk.X)
-
-img_btn_continue = tk.Button(button_frame, text="CONTINUE", command=close_window)
-img_btn_continue.pack(pady=25, fill=tk.X)
-
-# Show save button and set the button position
-img_btn_save = tk.Button(button_frame, text="SAVE "
-                                            "CANNY IMAGE", command=edged_img_maker.save_edged_img)
-
-# Bind the 'q' key to the close_window function
-root.bind("<q>", close_window)
-root.bind("<Q>", close_window)
-
-# Loop the setup
-root.mainloop()
-
-# Save the final result
-original_img = edged_img_maker.get_original_img()
-final_edged_img = edged_img_maker.get_edged_img()
+    def get_final_images(self):
+        return self.final_original_img, self.final_edged_img
